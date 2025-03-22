afdb48e3745f2054fe4d375feed44d5059146ed1
Pedro Alves <pedro.alves@escs.edu.br>
2025-03-22 00:13:27 -0300

    Atualizando o nome do arquivo e o nome dos funcionarios
    Houve essa pequena falha que precisou ser corrigida, mas foram alterações bem pontuais e sem grandes importâncias.

--- mapa_preceptores/mapa_preceptores.py
+++ mapa_preceptores/mapa_preceptores.py
@@ -5,10 +5,10 @@ mapa_df = folium.Map(location=[-15.7801, -47.9292], zoom_start=11)
 
 # Lista de coordenadas dos funcionários (latitude, longitude) com nomes
 coordenadas_funcionarios = [
-    ("Pedro Henrique", -15.8751, -48.0817),  # Samambaia Norte
+    ("Bianca", -15.8751, -48.0817),  # Samambaia Norte
     ("Lucas", -15.641994898394096, -47.797991007838036),  # Sobradinho
-    ("Itamara", -16.0204, -48.0665),  # Gama
-    ("Emilaine", -15.83819625405043, -48.02766793312688)  # Águas Claras
+    ("João", -16.0204, -48.0665),  # Gama
+    ("Maria", -15.83819625405043, -48.02766793312688)  # Águas Claras
 ]
 
 # Adiciona os marcadores ao mapa
